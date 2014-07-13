d508b63af3bf62b105fbdd081e85514e9233538f
admigmarcelo <admigmarcelo@gmail.com>
2014-07-13 15:47:24 -0300

    atualização da tela de login e teste de acesso, autenticação e confirmação de usuário

---
+++ TCC-GamificationJava/src/com/unigranrio/tcc/security/AutorizacaoInterceptor.java
@@ -0,0 +1,10 @@
+package com.unigranrio.tcc.security;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AutorizacaoInterceptor {
+
+}
